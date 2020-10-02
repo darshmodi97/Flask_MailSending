a43e9ef36c4be12d847492b8729df76a3ae8eff4
Darsh Modi <darsh.modi1111@gmail.com>
2020-10-02 19:16:40 +0530

    fixed issue of conection refuce error

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_mail import Mail,Message
 from flask import request,render_template,redirect
 app = Flask(__name__)
-mail = Mail(app)
+
 
 #### FLASK MAIL CONFIGURATION ######
 app.config['MAIL_SERVER']= 'smtp.gmail.com'
@@ -11,7 +11,7 @@ app.config['MAIL_USERNAME'] = 'darsh.modi1111@gmail.com'
 app.config['MAIL_PASSWORD'] = 'vnswpyprvlyuwesz'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-
+mail = Mail(app)
 ##########################################
 
 @app.route('/')
@@ -26,11 +26,12 @@ def sendmail():
         Body_data = request.form.get('body')
         
         msg = Message(
-            subject=sub,
+            sub,
             sender= 'darsh.modi1111@gmail.com',
-            recipients=email_to,
+            recipients=[email_to],
+            body=Body_data,
         )
-        msg.body = Body_data
+        
         mail.send(msg)
         return "Mail sent .."
     except Exception as e : 
